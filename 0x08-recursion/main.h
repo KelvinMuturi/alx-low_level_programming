d411273075ff9c8429fe896b0a2faf43c1fdfde9
KelvinMuturi <66muturikelvin@gmail.com>
2023-06-11 02:57:10 -0700

    correction

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,13 +1,15 @@
-#define MAIN_H
 #ifndef MAIN_H
+#define MAIN_H
 
 int _putchar(char c);
-int factorial(int n);
-void _puts_recursion(char *s);
-int _strlen_recursion(char *s);
-int is_prime_number(int n);
-int _pow_recursion(int x, int y);
-void _print_rev_recursion(char *s);
+void _sqrt_recursion(int n);
 int is_palindrome(char *s);
-int _sqrt_recursion(int n);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int _pow_recursion(int x, int y);
+int factorial(int n);
+int is_prime_number(int n);
+void _puts_recursion(char *s);
+
 #endif
+
