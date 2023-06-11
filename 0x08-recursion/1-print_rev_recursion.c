25c6135d3581dabb1dac08b33febc1b1dadf0751
KelvinMuturi <66muturikelvin@gmail.com>
2023-06-11 03:02:21 -0700

    correction 2

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,13 +1,10 @@
 #include "main.h"
-/**
- * _print_rev_recursion - Prints string in reverse.
- * @s: The string printed.
- */
+
 void _print_rev_recursion(char *s)
 {
-	if (*s == '\0')
+	if (*s)
 	{
-		_print_rev_recursion(s + 1);
 		_putchar(*s);
+		_print_rev_recursion(s + 1);
 	}
 }
